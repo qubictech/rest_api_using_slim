801a1f772912b05e716ef75f69593aa08adacec9
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-04-16 20:56:34 +0600

    connecting to mysql server

---
+++ includes/db_connect.php
@@ -0,0 +1,20 @@
+<?php
+
+class DbConnect
+{
+    private $conn;
+
+    function connect()
+    {
+        include_once dirname(__FILE__) . '/constants.php';
+
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect " . mysqli_connect_error();
+            return null;
+        }
+
+        return $this->conn;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,16 +1,24 @@
 <?php
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require '../vendor/autoload.php';
+require '../includes/db_connect.php';
 
 $app = new \Slim\App;
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    
+    $response->getBody()->write("Hello, $name<br>");    
+
+    $db = new DbConnect;
+
+    if ($db->connect() != null) {
+        echo "Connection Successful";
+    }
+
     return $response;
-});	
+});
 
 $app->run();
